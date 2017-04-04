f21c71130bffaa2bfd74c3258a480f6f8654f063
edock <edocoh@gmail.com>
2017-04-04 16:36:32 +0300

    adding files

---
+++ autoencoder.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+
+""" Auto Encoder Example.
+Using an auto encoder on MNIST handwritten digits.
+References:
+    Y. LeCun, L. Bottou, Y. Bengio, and P. Haffner. "Gradient-based
+    learning applied to document recognition." Proceedings of the IEEE,
+    86(11):2278-2324, November 1998.
+Links:
+    [MNIST Dataset] http://yann.lecun.com/exdb/mnist/
+"""
+from __future__ import division, print_function, absolute_import
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Import MNIST data
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data", one_hot=True)
+
+# Parameters
+normal_lambda = 1e-3
+learning_rate = 0.01
+training_epochs = 10
+batch_size = 256
+display_step = 1
+examples_to_show = 10
+
+# Network Parameters
+#n_hidden_1 = 256 # 1st layer num features
+#n_hidden_2 = 128 # 2nd layer num features
+n_hidden_1 = 784 # 1st layer num features
+n_hidden_2 = 784 # 2nd layer num features
+n_input = 784 # MNIST data input (img shape: 28*28)
+
+# tf Graph input (only pictures)
+X = tf.placeholder("float", [None, n_input])
+
+weights = {
+    'encoder_h1': tf.Variable(tf.random_normal([n_input, n_hidden_1])),
+    'encoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'decoder_h1': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_1])),
+    'decoder_h2': tf.Variable(tf.random_normal([n_hidden_1, n_input])),
+}
+biases = {
+    'encoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'encoder_b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'decoder_b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'decoder_b2': tf.Variable(tf.random_normal([n_input])),
+}
+
+
+# Building the encoder
+def encoder(x):
+    # Encoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']),
+                                   biases['encoder_b1']))
+    # Decoder Hidden layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']),
+                                   biases['encoder_b2']))
+    return layer_2
+
+
+# Building the decoder
+def decoder(x):
+    # Encoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']),
+                                   biases['decoder_b1']))
+    # Decoder Hidden layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']),
+                                   biases['decoder_b2']))
+    return layer_2
+
+# Construct model
+encoder_op = encoder(X)
+decoder_op = decoder(encoder_op)
+
+# Prediction
+y_pred = decoder_op
+# Targets (Labels) are the input data.
+y_true = X
+
+#print(encoder_op)
+#print(encoder_op.get_shape().as_list())
+#print(tf.norm(encoder_op, axis=1))
+#raw_input('bp0')
+
+t1 = tf.multiply(encoder_op, encoder_op)
+t2 = tf.reduce_sum(t1, 1)
+#t3 = tf.sqrt(t2)
+t4 = tf.reduce_mean(t2)
+# Define loss and optimizer, minimize the squared error
+cost = tf.reduce_mean(tf.pow(y_true - y_pred, 2)) + normal_lambda * t4
+optimizer = tf.train.RMSPropOptimizer(learning_rate).minimize(cost)
+
+# Initializing the variables
+init = tf.global_variables_initializer()
+
+# Launch the graph
+with tf.Session() as sess:
+    sess.run(init)
+    total_batch = int(mnist.train.num_examples/batch_size)
+    # Training cycle
+    for epoch in range(training_epochs):
+        # Loop over all batches
+        for i in range(total_batch):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+            # Run optimization op (backprop) and cost op (to get loss value)
+            _, c, norm_val = sess.run([optimizer, cost, t4], feed_dict={X: batch_xs})
+        # Display logs per epoch step
+        if epoch % display_step == 0:
+            print("Epoch:", '%04d' % (epoch+1),
+                  "cost=", "{:.9f}".format(c),
+                  "norm value=", "{:.9f}".format(norm_val))
+
+    print("Optimization Finished!")
+
+    # Applying encode and decode over test set
+    encode_decode = sess.run(
+        y_pred, feed_dict={X: mnist.test.images[:examples_to_show]})
+    # Compare original images with their reconstructions
+    f, a = plt.subplots(2, 10, figsize=(10, 2))
+    for i in range(examples_to_show):
+        a[0][i].imshow(np.reshape(mnist.test.images[i], (28, 28)))
+        a[1][i].imshow(np.reshape(encode_decode[i], (28, 28)))
+    f.show()
+    plt.draw()
+    plt.waitforbuttonpress()
+    X_train_embedded = sess.run(encoder_op, feed_dict={X: mnist.train.images})
+    X_test_embedded = sess.run(encoder_op, feed_dict={X: mnist.test.images})
+    data = (X_train_embedded, mnist.train.labels), (X_test_embedded, mnist.test.labels)
+    np.save('mnist_784_low_norm.npy', data)
